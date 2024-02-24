cb9195fca537bd33af42372abb531a95ec3b4036
matheusrodacki <44122620+matheusrodacki@users.noreply.github.com>
2024-02-24 04:08:37 -0300

    adiciona dependências e cria a classe base do projeto.

---
+++ buscar-cursos.php
@@ -0,0 +1,22 @@
+<?php
+
+require "vendor/autoload.php";
+require "src/Buscador.php";
+
+use Alura\BuscadorDeCursos\Buscador;
+use GuzzleHttp\Client;
+use Symfony\Component\DomCrawler\Crawler;
+
+$client = new Client(['base_uri' => 'https://alura.com.br/']);
+$crawler = new Crawler();
+
+$url = 'https://alura.com.br/cursos-online-programacao/php';
+
+$buscador = new Buscador($client, $crawler);
+$cursos = $buscador->buscar($url);
+
+foreach ($cursos as $curso) {
+
+    echo $curso . PHP_EOL;
+
+}
